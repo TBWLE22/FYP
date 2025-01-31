410e76240ee1bf5b94115819bd3343cea148e023
prg6useless <sainju.saral433@gmail.com>
2025-01-31 16:22:35 +0545

    add start filter butotn

--- client/src/Layout/Navbar.tsx
+++ client/src/Layout/Navbar.tsx
@@ -6,48 +6,48 @@ const Navbar = () => {
   const { pathname } = useLocation();
 
   return (
-      <div className="p-5">
-        <div className="sm:hidden">
-          <label htmlFor="Tab" className="sr-only">
-            Tab
-          </label>
+    <div className="p-5">
+      <div className="sm:hidden">
+        <label htmlFor="Tab" className="sr-only">
+          Tab
+        </label>
 
-          <select id="Tab" className="w-full rounded-md border-gray-200">
-            <option>Dashboard</option>
-            <option>Settings</option>
-          </select>
-        </div>
+        <select id="Tab" className="w-full rounded-md border-gray-200">
+          <option>Dashboard</option>
+          <option>Settings</option>
+        </select>
+      </div>
 
-        <div className="hidden sm:block">
-          <div className="border-b border-gray-200">
-            <nav className="-mb-px flex gap-6" aria-label="Tabs">
-              <Link
-                to="/"
-                className={`shrink-0 border-b-2 px-1 pb-4 text-lg font-medium
+      <div className="hidden sm:block">
+        <div className="border-b border-gray-200">
+          <nav className="-mb-px flex gap-6" aria-label="Tabs">
+            <Link
+              to="/"
+              className={`shrink-0 border-b-2 px-1 pb-4 text-lg font-medium
                   ${
                     pathname === "/"
                       ? "border-sky-500 text-sky-600"
                       : " text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   }`}
-              >
-                Dashbaord
-              </Link>
+            >
+              Dashboard
+            </Link>
 
-              <Link
-                to="/settings"
-                className={`shrink-0 border-b-2 px-1 pb-4 text-lg font-medium
+            <Link
+              to="/settings"
+              className={`shrink-0 border-b-2 px-1 pb-4 text-lg font-medium
                   ${
                     pathname === "/settings"
                       ? "border-sky-500 text-sky-600"
                       : " text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   }`}
-              >
-                Settings
-              </Link>
-            </nav>
-          </div>
+            >
+              Settings
+            </Link>
+          </nav>
         </div>
       </div>
+    </div>
   );
 };
 
--- client/src/Pages/Dashboard.tsx
+++ client/src/Pages/Dashboard.tsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <div className="mx-auto max-w-3xl text-center">
           <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-            IP Spoofing and DOS Attacks
+            IP Spoofing Detection
           </h2>
 
           <p className="mt-4 text-gray-500 sm:text-xl">
@@ -47,7 +47,7 @@ const Dashboard = () => {
 
           <div className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center">
             <dt className="order-last text-lg font-medium text-gray-500">
-              Incoming/Outgoing Packets
+              Incoming Packets
             </dt>
 
             <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
